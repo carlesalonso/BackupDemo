664feb3013c06d9240542bad812b8e91ce5f0822
Carles Alonso Martinez <carlos.martinez@mataro.epiaedu.cat>
2023-12-11 09:10:19 +0100

    Create fullBackup.sh

---
+++ fullBackup.sh
@@ -0,0 +1,33 @@
+
+#!/bin/bash
+
+# munta unitat
+mount /dev/sdb1/   /media/backup
+
+# test
+if [ $? -ne 0 ]; then
+  echo "$(date +%F-%T) Error muntat unitat" >> /var/log/backup
+  exit 1
+fi
+
+# set PASSPHRASE
+PASSPHRASE='12345678'
+export PASSPHRASE
+
+# acció backup
+duplicity full /var/www file:///media/backup/
+
+# test
+if [ $? -ne 0 ]; then
+  echo "$(date +%F-%T) Error durant backup" >> /var/log/backup
+  exit 1
+fi
+
+# unset PASSPHRASE
+unset PASSPHRASE
+
+# desmuntar unitat
+umount /media/backup
+
+echo "$(date +%F-%T) Backup realitzat exitosament" >> /var/log/backup
+exit 0
